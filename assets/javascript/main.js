234f0def9d348159f79001e3a1fb9d4f6edc1e71
tina tong <tinale0414@gmail.com>
2017-12-23 09:45:39 -0800

    final

---
+++ assets/javascript/main.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+	// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAzit10bhuAQZg-BV59gnwxWS7rsrSSP1o",
+    authDomain: "fir-hw-3d865.firebaseapp.com",
+    databaseURL: "https://fir-hw-3d865.firebaseio.com",
+    projectId: "fir-hw-3d865",
+    storageBucket: "",
+    messagingSenderId: "493834836827"
+  };
+  firebase.initializeApp(config);
+	  var database = firebase.database();
+	// 2. Button for adding Trains
+	$("#addTrainBtn").on("click", function(){
+
+		// Grabs user input and assign to variables
+		var trainName = $("#trainNameInput").val().trim();
+		var destination = $("#destinationInput").val().trim();
+		var trainTimeInput = moment($("#trainTimeInput").val().trim(), "HH:mm").subtract(1, "years").format("X");
+		var frequencyInput = $("#frequencyInput").val().trim();
+
+		// Test for variables entered
+		console.log(trainName);
+		console.log(destination);
+		console.log(trainTimeInput);
+		console.log(frequencyInput);
+
+		// Creates local "temporary" object for holding train data
+		// Will push this to firebase
+		var newTrain = {
+			name:  trainName,
+			line: lineName,
+			destination: destination,
+			trainTime: trainTimeInput,
+			frequency: frequencyInput,
+		}
+
+		// pushing trainInfo to Firebase
+		trainData.push(newTrain);
+
+		// clear text-boxes
+		$("#trainNameInput").val("");
+		$("#lineInput").val("");
+		$("#destinationInput").val("");
+		$("#trainInput").val("");
+		$("#frequencyInput").val("");
+
+		// Prevents page from refreshing
+		return false;
+	});
+
+	$(document).on("click", "#submit", function(event) {
+		event.preventDefault();
+	
+		console.log(childSnapshot.val());
+
+		// assign firebase variables to snapshots.
+		var firebaseName = childSnapshot.val().name;
+		var firebaseLine = childSnapshot.val().line;
+		var firebaseDestination = childSnapshot.val().destination;
+		var firebaseTrainTimeInput = childSnapshot.val().trainTime;
+		var firebaseFrequency = childSnapshot.val().frequency;
+		
+		var diffTime = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes");
+		var timeRemainder = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes") % firebaseFrequency ;
+		var minutes = firebaseFrequency - timeRemainder;
+
+		var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A"); 
+		
+		// Test for correct times and info
+		console.log(minutes);
+		console.log(nextTrainArrival);
+		console.log(moment().format("hh:mm A"));
+		console.log(nextTrainArrival);
+		console.log(moment().format("X"));
+
+		// Append train info to table on page
+		$("#trainTable > tbody").append("<tr><td>" + firebaseName + "</td><td>" + firebaseLine + "</td><td>"+ firebaseDestination + "</td><td>" + firebaseFrequency + " mins" + "</td><td>" + nextTrainArrival + "</td><td>" + minutes + "</td></tr>");
+
+	});
+});
